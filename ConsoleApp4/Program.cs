6546b25944c934ecc3813d6db441d83007a48b7b
IWTDIRL <sheydaev.emil@mail.ru>
2023-10-18 15:07:56 +0300

    update

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -9,6 +9,15 @@ namespace Matrices
         {
             Console.Write("Введите размерность матрицы: ");
             int razmer = int.Parse(Console.ReadLine());
+            if (razmer == 0 || razmer < 0 || razmer == 1)
+            {
+                Console.WriteLine("Такого размера матрицы быть не может. Попробуйте ещё раз.");
+                Console.ReadKey();
+                Console.Clear();
+                Main();
+            }
+            else 
+            {
 
             int[,] a = new int[razmer, razmer];
             int[,] b = new int[razmer, razmer];
@@ -19,55 +28,56 @@ namespace Matrices
             Console.WriteLine("Введите значения для матрицы b:");
             vvod(b, razmer);
 
-            while (true)
-            {
-                Console.WriteLine("\nВыберите действие: \n");
-                Console.WriteLine("1. Вывести матрицу a");
-                Console.WriteLine("2. Вывести матрицу b");
-                Console.WriteLine("3. Сложить матрицы a и b");
-                Console.WriteLine("4. Вычесть матрицу a из b");
-                Console.WriteLine("5. Вычесть матрицу b из a");
-                Console.WriteLine("6. Выйти");
-                int vybor = int.Parse(Console.ReadLine());
-
-                switch (vybor)
+                while (true)
                 {
-                    case 1:
-                        Console.Clear();
-                        Console.WriteLine("Матрица a:");
-                        vyvod(a, razmer);
-                        break;
-                    case 2:
-                        Console.Clear();
-                        Console.WriteLine("Матрица b:");
-                        vyvod(b, razmer);
-                        break;
-                    case 3:
-                        Console.Clear();
-                        int[,] summa = Plus(a, b, razmer);
-                        Console.WriteLine("a + b = ");
-                        vyvod(summa, razmer);
-                        break;
-                    case 4:
-                        Console.Clear();
-                        int[,] raznost = aminusb(a, b, razmer);
-                        Console.WriteLine("a - b = ");
-                        vyvod(raznost, razmer);
-                        break;
-                    case 5:
-                        Console.Clear();
-                        int[,] raznost1 = bminusa(a, b, razmer);
-                        Console.WriteLine("b - a = ");
-                        vyvod(raznost1, razmer);
-                        break;
-                    case 6:
-                        Console.Clear();
-                        Console.WriteLine("Выход");
-                        return;
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Некорректный выбор. Попробуйте выбрать ещё раз");
-                        break;
+                    Console.WriteLine("\nВыберите действие: \n");
+                    Console.WriteLine("1. Вывести матрицу a");
+                    Console.WriteLine("2. Вывести матрицу b");
+                    Console.WriteLine("3. Сложить матрицы a и b");
+                    Console.WriteLine("4. Вычесть матрицу a из b");
+                    Console.WriteLine("5. Вычесть матрицу b из a");
+                    Console.WriteLine("6. Выйти");
+                    int vybor = int.Parse(Console.ReadLine());
+
+                    switch (vybor)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("Матрица a:");
+                            vyvod(a, razmer);
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Матрица b:");
+                            vyvod(b, razmer);
+                            break;
+                        case 3:
+                            Console.Clear();
+                            int[,] summa = Plus(a, b, razmer);
+                            Console.WriteLine("a + b = ");
+                            vyvod(summa, razmer);
+                            break;
+                        case 4:
+                            Console.Clear();
+                            int[,] raznost = aminusb(a, b, razmer);
+                            Console.WriteLine("a - b = ");
+                            vyvod(raznost, razmer);
+                            break;
+                        case 5:
+                            Console.Clear();
+                            int[,] raznost1 = bminusa(a, b, razmer);
+                            Console.WriteLine("b - a = ");
+                            vyvod(raznost1, razmer);
+                            break;
+                        case 6:
+                            Console.Clear();
+                            Console.WriteLine("Выход");
+                            return;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Некорректный выбор. Попробуйте выбрать ещё раз");
+                            break;
+                    }
                 }
             }
         }
